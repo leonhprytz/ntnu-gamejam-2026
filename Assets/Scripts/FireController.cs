052d285338d6eb525da98f7609bc8db5246df5cc
Sebastian Bugge <sebastian.bugge@bekk.no>
2026-09-19 15:09:30 +0200

    Refactor BonfireManager.

--- Assets/Scripts/FireController.cs
+++ Assets/Scripts/FireController.cs
@@ -1,42 +1,22 @@
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
-public enum FireState
-{
-    Small,
-    Medium,
-    Large,
-}
-
 public class FireController : MonoBehaviour
 {
     private Light2D light;
 
-    public FireState state = FireState.Small;
+    public BonfireSize fireSize = BonfireSize.Small;
     public float[] radii = new float[] { 1.85F, 3.2F, 5.0F };
     public float flickerVariation = 0.2F;
     public float flickerSpeed = 2;
 
     private void updateLightRadius()
     {
-        float flickerRange = flickerVariation * radii[(int)state];
+        float flickerRange = flickerVariation * radii[(int)fireSize];
         float variation =
             Mathf.PerlinNoise1D(flickerSpeed * Time.time) * flickerRange - flickerRange / 2;
 
-        light.pointLightOuterRadius = radii[(int)state] + variation;
-    }
-
-    public void increaseState()
-    {
-        switch (this.state)
-        {
-            case (FireState.Small):
-                state = FireState.Medium;
-                break;
-            case (FireState.Medium):
-                state = FireState.Large;
-                break;
-        }
+        light.pointLightOuterRadius = radii[(int)fireSize] + variation;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
